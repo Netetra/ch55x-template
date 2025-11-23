fc9f8e27943b70ebe83b88ca37c97d07062536fd
Netetra <netetra07@gmail.com>
2025-11-23 19:36:07 +0900

    feat: デバイスにinを要求する例

--- examples/usbh.c
+++ examples/usbh.c
@@ -1,21 +1,39 @@
 #define USE_UART0_STDIO
-// #define ENABLE_DEBUG_LOG
+#define ENABLE_DEBUG_LOG
 
 #include <stdio.h>
 #include <ch559.h>
 #include <usb/host.h>
 
+#define EP_NUM 2
+
+__code struct SetupRequest set_configuration_req = {
+  .bRequestType = SETUP_REQUEST_DIR_OUT,
+  .bRequest = SET_CONFIGURATION,
+  .wValue = 1,
+  .wIndex = 0,
+  .wLength = 0
+};
+
 uint8_t connected_handler(uint8_t hub, struct DeviceDesc* device_desc) {
-  printf("hub%d: VID 0x%04X , PID 0x%04X\n", hub, device_desc->idVendor, device_desc->idProduct);
+  uint8_t error = usbh_transfer_control(hub, &set_configuration_req, 0, 0, 0);
+  if (error) { return true; }
+  DEBUG("hub%d: set configuration 1\n", hub);
   return false;
 }
 
-void disconnected_handler(uint8_t hub) {
-  printf("hub%d: disconnected\n", hub);
-}
+void disconnected_handler(uint8_t hub) {}
 
 void poll_handler(uint8_t hub) {
-  printf("hub%d: poll\n", hub);
+  uint8_t ep_pid = (PACKET_ID_IN << 4) | EP_NUM;
+  usbh_select_port(hub);
+  uint8_t error = usbh_transfer(ep_pid, AUTO_TOGGLE, 1);
+  if (error) { return; }
+  printf("hub%d: ", hub);
+  for (uint8_t i = 0; i < USB_RX_LEN; i++) {
+    printf("0x%02X  ", rx_buffer[i]);
+  }
+  printf("\n");
 }
 
 void main(void) {
