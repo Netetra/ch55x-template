862cb748b283be1b52f4a7b425b4e79c4f4b9e17
Netetra <netetra07@gmail.com>
2025-12-01 20:58:41 +0900

    feat: usbマウスをポーリングする例

--- examples/usbh_mouse_poll.c
+++ examples/usbh_mouse_poll.c
@@ -5,6 +5,18 @@
 #include <ch559.h>
 #include <usb/host.h>
 
+#define VENDOR_ID  0x30FA
+#define PRODUCT_ID 0x0300
+#define ENDPOINT   1
+
+struct Report {
+  uint8_t left: 1;
+  uint8_t right: 1;
+  uint8_t wheel: 1;
+  uint8_t padding: 3;
+  uint32_t x_y_scroll;
+};
+
 __code struct SetupRequest set_configuration_req = {
   .bRequestType = SETUP_REQUEST_DIR_OUT,
   .bRequest = SET_CONFIGURATION,
@@ -16,6 +28,7 @@ __code struct SetupRequest set_configuration_req = {
 uint8_t buffer[64];
 
 uint8_t connected_handler(uint8_t hub, struct DeviceDesc* device_desc) {
+  if (device_desc->idVendor != VENDOR_ID || device_desc->idProduct != PRODUCT_ID) { return true; }
   uint8_t error = usbh_transfer_control(hub, &set_configuration_req, 0, 0, 0);
   if (error) { return true; }
   DEBUG("hub%d: set configuration 1\n", hub);
@@ -26,13 +39,18 @@ void disconnected_handler(uint8_t hub) {}
 
 void poll_handler(uint8_t hub) {
   uint16_t len;
-  uint8_t error = usbh_transfer_in(hub, 2, buffer, &len, 1);
+  uint8_t error = usbh_transfer_in(hub, 1, buffer, &len, 1);
   if (error) { return; }
+  if (len != sizeof(struct Report)) { return; }
+  struct Report* report = (struct Report*)buffer;
+  int16_t x_12bit = report->x_y_scroll & 0x00000FFF;
+  int16_t x = (x_12bit & 0x800) ? (x_12bit | 0xF000) : (x_12bit & 0x0FFF);
+  int16_t y_12bit = (report->x_y_scroll & 0x00FFF000) >> 12;
+  int16_t y = (y_12bit & 0x800) ? (y_12bit | 0xF000) : (y_12bit & 0x0FFF);
+  int8_t scroll = (report->x_y_scroll & 0xFF000000) >> 24;
   printf("hub%d: ", hub);
-  for (uint8_t i = 0; i < len; i++) {
-    printf("0x%02X  ", buffer[i]);
-  }
-  printf("\n");
+  printf("left %d, right %d, wheel %d, ", report->left, report->right, report->wheel);
+  printf("x %4d, y %4d, scroll %4d\n", x, y, scroll);
 }
 
 void main(void) {
